83326d4179ef2974f51bbe68e0667533cbf93611
jawad Bin Azam <abidibnazam@gmail.com>
2023-11-30 16:43:19 +0600

    get and delete method

--- src/Blocks/TodoSections.jsx
+++ src/Blocks/TodoSections.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TodoBlcoks from "../components/TodoBlcoks";
+import axios from "axios";
+import { baseUrl } from "../Api/api";
 
 const TodoSections = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/pending`)
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, [baseUrl]);
+
+  console.log(data);
   return (
     <div className="px-20 pt-10 h-screen">
       {/* 3 grids for todo status */}
-      <div className="grid grid-cols-3">
-        <div className="border border-slate-500 h-60"></div>
-        <div className="border border-slate-500 h-60"></div>
-        <div className="border border-slate-500 h-60"></div>
+      <div className="grid grid-cols-3 gap-5">
+        <div>
+          <h1 className="px-2 py-3 font-sans text-md font-bold bg-indigo-400 text-white text-center">
+            Pending
+          </h1>
+          <div className="mt-3">
+            {data.map((todo, i) => {
+              return (
+                <TodoBlcoks
+                  key={i}
+                  title={todo.title}
+                  desc={todo.desc}
+                  stat={todo.stat}
+                  id={todo._id}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h1 className="px-2 py-3 font-sans text-md font-bold bg-emerald-400 text-white text-center">
+            Finished
+          </h1>
+        </div>
+        <div>
+          <h1 className="px-2 py-3 font-sans text-md font-bold bg-red-400 text-white text-center">
+            Cancelled
+          </h1>
+        </div>
       </div>
     </div>
   );
--- src/components/TodoBlcoks.jsx
+++ src/components/TodoBlcoks.jsx
@@ -1,7 +1,39 @@
+import axios from "axios";
 import React from "react";
+import { baseUrl } from "../Api/api";
 
-const TodoBlcoks = () => {
-  return <div className="rounded-lg"></div>;
+const TodoBlcoks = ({ title, desc, stat, id }) => {
+  const deleteOne = (idParam) => {
+    console.log(idParam);
+    axios
+      .delete(`${baseUrl}/pending/${idParam}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="rounded-lg bg-[#EEEEEC] py-4 px-3 my-2">
+      <h1 className="text-sm font-sans">{title}</h1>
+      <div className="border-b border-slate-950 px-0"></div>
+      <p className="pt-3 text-xs  py-2 rounded-md">{desc}</p>
+      <div className="border-b border-slate-800 px-0"></div>
+      <div className="flex justify-between pt-3">
+        {stat === "pending" && (
+          <button className="text-xs px-4 py-2 rounded-md bg-black text-white font-bold hover:bg-sky-400">
+            Finsihed
+          </button>
+        )}
+        <button className="text-xs px-4 py-2 rounded-md bg-black text-white font-bold hover:bg-red-400">
+          Cancel
+        </button>
+        <button
+          onClick={() => deleteOne(id)}
+          className="text-xs px-4 py-2 rounded-md bg-black text-white font-bold hover:bg-emerald-400"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoBlcoks;
